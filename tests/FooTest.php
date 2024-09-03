dab7ee536b74f14627c7739534ea36a972aebd34
jrfnl <jrfnl@users.noreply.github.com>
2024-09-03 14:11:38 +0200

    Add `#[CoversTrait]`
    PHPUnit 10 fails with a fatal "Attribute class not found", exit code 255.
    
    PHPUnit 11 throws PHPUnit deprecation notice, exit code 1.

--- tests/FooTest.php
+++ tests/FooTest.php
@@ -5,8 +5,10 @@ namespace Jrf\PHPUnit1011\Example\Tests;
 use Exception;
 use Jrf\PHPUnit1011\Example\Foo;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\CoversTrait;
 use PHPUnit\Framework\TestCase;
 
+#[CoversTrait(Foo::class)]
 #[CoversClass(Foo::class)]
 final class FooTest extends TestCase
 {
